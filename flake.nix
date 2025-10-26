9f046406706b75c8b34c0434f35242e2b0065844
Kevin Quick <kquick@galois.com>
2025-10-26 12:12:51 -0700

    Simplify flake: single nixpkgs, new haskellShells, and full test+doc builds.

--- flake.nix
+++ flake.nix
@@ -10,8 +10,7 @@
   nixConfig.bash-prompt-suffix = "tasty-checklist.env} ";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    nixpkgs2411.url = github:nixos/nixpkgs/24.11;  # GHC8.10--GHC9.12
+    nixpkgs.url = github:nixos/nixpkgs;
     levers = {
       type = "github";
       owner = "kquick";
@@ -32,91 +31,27 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs2411, levers
+  outputs = { self, nixpkgs, levers
             , hedgehog-classes-src
             , parameterized-utils-src
             , tasty-hedgehog-src
             }:
-     let shellWith = pkgs: adds: drv: drv.overrideAttrs(old:
-           { buildInputs = old.buildInputs ++ adds pkgs; });
-         shellPkgs = pkgs: [ pkgs.cabal-install
-                           ];
-     in
     rec
     {
-      defaultPackage = levers.eachSystem (s:
-        self.packages.${s}.tasty-checklist.default);
-
-      devShell = levers.eachSystem (s:
-        let pkgs = import nixpkgs { system=s; };
-            in shellWith pkgs shellPkgs defaultPackage.${s}.env);
-
-      devShells =
-        let oneshell = s: n:
-              let pkgs = import nixpkgs { system=s; };
-              in levers.variedTargets
-                { ghcver = levers.validGHCVersions pkgs.haskell.compiler; }
-                ( { ghcver, ... } @ vargs:
-                  shellWith pkgs shellPkgs
-                    (self.packages.${s}.${n}.${ghcver}.env)
-                );
-        in
-          levers.eachSystem
-            (s:
-              let pkgs = import nixpkgs { system=s; };
-                  names = builtins.attrNames (self.packages.${s});
-                  shells = pkgs.lib.genAttrs names (oneshell s);
-              in shells
-                 // { default = devShells.${s}.tasty-checklist_test.default; }
-            ) ;
+      devShells = levers.haskellShells
+        { inherit nixpkgs;
+          flake = self;
+          defaultPkg = "tasty-checklist";
+          # additionalPackages = pkgs.haskell.packages.ghc8107.profiteur
+        };
 
       packages = levers.eachSystem (system:
-        let nixpkgs_list = [
-              nixpkgs2411
-              nixpkgs
-            ];
-            mkHaskell = name: src: overDrvOrArgs:
-              levers.mkHaskellPkgs {
-                inherit system;
-              } nixpkgs_list name src overDrvOrArgs;
+        let mkHaskell = levers.mkHaskellPkg {inherit nixpkgs system;};
             pkgs = import nixpkgs { inherit system; };
-            haskellAdj = drv:
-              with (pkgs.haskell).lib;
-              dontHaddock (
-                dontCheck (
-                  dontBenchmark (
-                    # disableLibraryProfiling (
-                    #   disableExecutableProfiling
-                        drv)
-                    # )
-                )
-              );
-            wrap = levers.pkg_wrapper system pkgs;
         in rec {
-          default = tasty-checklist.ghc910;
-          TESTS = wrap "tasty-checklist-TESTS" [
-            tasty-checklist_test         # current version
-            tasty-checklist_test.ghc810  # minimum version
-            tasty-checklist_test.ghc912  # maximum version
-          ];
-          DOC = wrap "tasty-checklist-DOC" [
-            tasty-checklist_doc         # current version
-            tasty-checklist_doc.ghc810  # minimum version
-            tasty-checklist_doc.ghc912  # maximum version
-          ];
+          default = tasty-checklist;
           tasty-checklist = mkHaskell "tasty-checklist" self {
             inherit parameterized-utils;
-            adjustDrv = args: haskellAdj;
-          };
-          tasty-checklist_test = mkHaskell "tasty-checklist" self {
-            inherit parameterized-utils;
-            adjustDrv = args: drv:
-              with pkgs.haskell.lib; doBenchmark (doCheck (haskellAdj drv));
-          };
-          tasty-checklist_doc = mkHaskell "tasty-checklist" self {
-            inherit parameterized-utils;
-            adjustDrv = args: drv:
-              with pkgs.haskell.lib.doHaddock; (haskellAdj drv);
           };
           hedgehog-classes = mkHaskell "hedgehog-classes" hedgehog-classes-src {};
           parameterized-utils = mkHaskell "parameterized-utils"
@@ -127,8 +62,5 @@
             };
           tasty-hedgehog = mkHaskell "tasty-hedgehog" tasty-hedgehog-src {};
         });
-
-      checks = levers.eachSystem (system:
-        self.packages."${system}".TESTS);
     };
 }
