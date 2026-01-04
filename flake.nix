a48cddc0a3a0b1ae4ab8255f1e4a8d8dc732500c
Kevin Quick <kquick@galois.com>
2026-01-04 15:50:52 -0800

    Flake updates: lock parameterized-utils at 6cd1f32.

--- flake.nix
+++ flake.nix
@@ -18,7 +18,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     parameterized-utils-src = {
-      url = github:GaloisInc/parameterized-utils;
+      # Lock to revision 6cd1f32 because the subsequent changes replace lens with
+      # microlens-pro, and the latter was only introduced in Feb 2024 (circa GHC
+      # 9.8.3), so it's not available for older nixpkgs configurations.  As of
+      # 2025 Dec, there are no functional changes in parameterized-utils, just
+      # dependency changes, and there is intent to remove the microlens-pro
+      # dependency from parameterized-utils, so freeze this until that occurs.
+      url = "github:GaloisInc/parameterized-utils/6cd1f32";
       flake = false;
     };
   };
