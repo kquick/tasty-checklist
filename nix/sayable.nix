e8db8def130843fbad5c8804978bcae79305ee21
Kevin Quick <kquick@galois.com>
2026-09-13 14:10:25 -0700

    Add pre-calculated cabal2nix-based derivations, updated via eng flake_update

---
+++ nix/sayable.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, base, containers, doctest, exceptions, HUnit, lib
+, parameterized-utils, tasty, tasty-expected-failure, tasty-hunit
+, text
+}:
+mkDerivation {
+  pname = "tasty-checklist";
+  version = "1.0.10.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base containers exceptions parameterized-utils text
+  ];
+  testHaskellDepends = [
+    base doctest HUnit parameterized-utils tasty tasty-expected-failure
+    tasty-hunit text
+  ];
+  description = "Check multiple items during a tasty test";
+  license = lib.licenses.isc;
+}
